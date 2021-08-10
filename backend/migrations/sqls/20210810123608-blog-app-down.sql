d2707a8b0e3ff06a4d31981fc236330d2d19b8f7
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-08-10 14:45:54 +0200

    started migrations

---
+++ backend/migrations/sqls/20210810123608-blog-app-down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS imageInNews;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS news;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS images;
+
+/* Replace with your SQL commands */
