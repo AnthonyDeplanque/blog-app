d11fabd71b6d9125578bbc31e9ac628c508656df
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-08-11 18:02:28 +0200

    login user controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -16,47 +16,32 @@ const postUser = async (req, res) => {
     bio,
     image,
   } = req.body;
-};
-const hashedPassword = await argon2.hash(password);
-const error = Joi.object(usersMiddleware.postUserValidationObject).validate(
-  {
-    nickName,
-    email,
-    hashedPassword,
-    role,
-    firstName,
-    lastName,
-    date,
-    bio,
-    image,
-  },
-  { abortEarly: false }
-);
-if (error) {
-  console.error(error);
-  res.status(422).json({ validationError: error.details });
-} else {
-  usersModel.getOneUserQueryByEmail(email).then(([result]) => {
-    if (result.length) {
-      res.status(409).send("Email already used");
-    } else {
-      usersModel
-        .addUserQuery({
-          nickname,
-          email,
-          hashedPassword,
-          role,
-          firstName,
-          lastName,
-          date,
-          bio,
-          image,
-        })
-        .then((results) => {
-          const idUser = results.insertId();
-          const createdUser = {
-            idUser,
-            nickName,
+  const hashedPassword = await argon2.hash(password);
+  const error = Joi.object(usersMiddleware.postUserValidationObject).validate(
+    {
+      nickName,
+      email,
+      hashedPassword,
+      role,
+      firstName,
+      lastName,
+      date,
+      bio,
+      image,
+    },
+    { abortEarly: false }
+  );
+  if (error) {
+    console.error(error);
+    res.status(422).json({ validationError: error.details });
+  } else {
+    usersModel.getOneUserQueryByEmail(email).then(([result]) => {
+      if (result.length) {
+        res.status(409).send("Email already used");
+      } else {
+        usersModel
+          .addUserQuery({
+            nickname,
             email,
             hashedPassword,
             role,
@@ -65,13 +50,63 @@ if (error) {
             date,
             bio,
             image,
-          };
-          res.status(201).json(createdUser);
+          })
+          .then((results) => {
+            const idUser = results.insertId();
+            const createdUser = {
+              idUser,
+              nickName,
+              email,
+              hashedPassword,
+              role,
+              firstName,
+              lastName,
+              date,
+              bio,
+              image,
+            };
+            res.status(201).json(createdUser);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("error creating a User");
+          });
+      }
+    });
+  }
+};
+
+const loginUser = (req, res) => {
+  const { credentialEmail, credentialPassword } = req.body;
+  usersModel
+    .getHashedPasswordByEmail(credentialEmail)
+    .then(async ([[results]]) => {
+      argon2
+        .verify(result.hashedPassword, credentialPassword)
+        .then((match) => {
+          if (match) {
+            usersModel
+              .getOneUserQueryByEmail(credentialEmail)
+              .then(([[results]]) => {
+                const token = jwtServices.createToken(results.id); // not yet tested
+                res
+                  .status(200)
+                  .json({
+                    ...results,
+                    token: token,
+                    message: "ACCESS_GRANTED",
+                  });
+              });
+          } else {
+            res.status(401).json({ message: "ACCESS_DENIED" });
+          }
         })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send("error creating a User");
-        });
-    }
-  });
+        .catch((error) => console.error(error));
+    })
+    .catch((error) => console.error(error));
+};
+
+module.exports = {
+  postUser,
+  loginUser,
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const {postUser} = require('../controllers/users');
+const {postUser, loginUser} = require('../controllers/users');
 
 const router = express.Router();
 
 router.post('/', postUser);
+router.post('/auth/', loginUser);
 
 module.exports = router;
