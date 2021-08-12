db5b7d45534a8806aa4fd122ff3b93465ada13e5
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-08-12 17:46:05 +0200

    GET routes for users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,7 +4,6 @@ const usersMiddleware = require("../middlewares/users");
 const argon2 = require("argon2");
 const jwtServices = require("../services/jwt");
 
-
 const postUser = async (req, res) => {
   const {
     nickName,
@@ -23,15 +22,15 @@ const postUser = async (req, res) => {
     usersMiddleware.postUserValidationObject
   ).validate(
     {
-    nickName,
-    email,
-    hashedPassword,
-    role,
-    firstName,
-    lastName,
-    date,
-    bio,
-    image,
+      nickName,
+      email,
+      hashedPassword,
+      role,
+      firstName,
+      lastName,
+      date,
+      bio,
+      image,
     },
     { abortEarly: false }
   );
@@ -43,7 +42,7 @@ const postUser = async (req, res) => {
       .getOneUserQueryByEmail(email)
       .then(([results]) => {
         if (results.length) {
-          res.status(409).json({message:"email already in use"});
+          res.status(409).json({ message: "email already in use" });
         } else {
           usersModel
             .addUserQuery({
@@ -70,11 +69,13 @@ const postUser = async (req, res) => {
                 bio,
                 image,
               };
-              res.status(201).json({...createdUser, message:"user successfully created"} );
+              res
+                .status(201)
+                .json({ ...createdUser, message: "user successfully created" });
             })
             .catch((err) => {
               console.error(err);
-              res.status(500).json({message:"error creating a user"});
+              res.status(500).json({ message: "error creating a user" });
             });
         }
       })
@@ -84,21 +85,21 @@ const postUser = async (req, res) => {
 
 const loginUser = (req, res) => {
   const { email, password } = req.body;
-  const {error} = Joi.object(usersMiddleware.loginUserValidationObject).validate({email, password}, {abortEarly:false});
+  const { error } = Joi.object(
+    usersMiddleware.loginUserValidationObject
+  ).validate({ email, password }, { abortEarly: false });
   if (error) {
     console.error(error);
     res.status(422).json({ validationError: error.details });
   } else {
-  usersModel
-    .getHashedPasswordByEmail(email)
-    .then(async ([[results]]) => {
-      argon2
-        .verify(results.hashedPassword, password)
-        .then((match) => {
-          if (match) {
-            usersModel
-              .getOneUserQueryByEmail(email)
-              .then(([[result]]) => {
+    usersModel
+      .getHashedPasswordByEmail(email)
+      .then(async ([[results]]) => {
+        argon2
+          .verify(results.hashedPassword, password)
+          .then((match) => {
+            if (match) {
+              usersModel.getOneUserQueryByEmail(email).then(([[result]]) => {
                 const token = jwtServices.createToken(result.email); // not yet tested
                 res.status(200).json({
                   ...result,
@@ -106,16 +107,70 @@ const loginUser = (req, res) => {
                   message: "ACCESS_GRANTED",
                 });
               });
-          } else {
-            res.status(401).json({ message: "ACCESS_DENIED" });
-          }
-        })
-        .catch((error) => console.error(error));
-    })
-    .catch((error) => {console.error(error); res.status(404).send("user not found")});
-}};
+            } else {
+              res.status(401).json({ message: "ACCESS_DENIED" });
+            }
+          })
+          .catch((error) => console.error(error));
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(404).json({ message: "user not found" });
+      });
+  }
+};
 
+const getAllUsers = (req, res) => {
+  const { first, last, email } = req.query;
+  if (email) {
+    usersModel
+      .getOneUserQueryByEmail(email)
+      .then(([results]) => {
+        res.status(200).json(results);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(404).json({ message: `user with email ${email} not found` });
+      });
+  }
+  if (first && last) {
+    usersModel
+      .getSelectedUsersQuery(first, last)
+      .then(([results]) => {
+        res.status(200).json(results);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ message: error });
+      });
+  } else {
+    usersModel
+      .getUsersQuery()
+      .then(([results]) => res.status(200).json(results))
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ message: error });
+      });
+  }
+};
+
+const getOneUserById = (req, res) => {
+  const { id } = req.params;
+  usersModel
+    .getOneUserQueryById(id)
+    .then(([results]) => {
+      if (results.length) {
+        res.status(200).json(results);
+      } else res.status(404).json({ message: `user with id ${id} not found` });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: error });
+    });
+};
 module.exports = {
   postUser,
   loginUser,
+  getAllUsers,
+  getOneUserById,
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const {postUser, loginUser} = require('../controllers/users');
+const {postUser, loginUser, getAllUsers, getOneUserById} = require('../controllers/users');
 
 const router = express.Router();
 
 router.post('/', postUser);
 router.post('/auth/', loginUser);
+router.get("/", getAllUsers);
+router.get('/:id', getOneUserById);
 
 module.exports = router;
