83f0cf57ff7fe0e011cee0cd64c0a405b6ea93c8
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-08-13 12:47:06 +0200

    categories middleware

---
+++ backend/middlewares/categories.js
@@ -0,0 +1,9 @@
+const Joi = require ('joi');
+
+const postCategoryValidationObject = {
+  title : Joi.string().max(255).required(),
+};
+const updateCategoryValidationObject = {
+  title : Joi.string().max(255);
+}
+module.exports ={postCategoryValidationObject, updateCategoryValidationObject};
