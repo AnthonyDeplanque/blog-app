de48f5bd20f4aa63149038c8df1d16c2c8b87506
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-08-14 16:41:01 +0200

    corrected indentation for models

--- backend/models/categories.js
+++ backend/models/categories.js
@@ -1,35 +1,35 @@
-const connection = require ('../db-config');
+const connection = require("../db-config");
 const db = connection.promise();
 
-const addCategoryQuery = (title) =>{
-  return db.query('INSERT INTO categories SET ?', [title]);
-}
+const addCategoryQuery = (title) => {
+  return db.query("INSERT INTO categories SET ?", [title]);
+};
 const getCategoriesQuery = () => {
-  return db.query('SELECT * FROM categories');
-}
+  return db.query("SELECT * FROM categories");
+};
 const getOneCategoryQueryById = (id) => {
-  return db.query('SELECT * FROM categories WHERE id = ?', [id]);
-}
-const getOneCategoryQueryByTitle = (title) =>{
-  return db.query('SELECT * FROM categories WHERE title = ?', [title]);
-}
-const getSelectedCategoriesQuery = (first, last)=>{
-  return db.query ('SELECT * FROM categories LIMIT ?, ?', [first, last]);
-}
+  return db.query("SELECT * FROM categories WHERE id = ?", [id]);
+};
+const getOneCategoryQueryByTitle = (title) => {
+  return db.query("SELECT * FROM categories WHERE title = ?", [title]);
+};
+const getSelectedCategoriesQuery = (first, last) => {
+  return db.query("SELECT * FROM categories LIMIT ?, ?", [first, last]);
+};
 
-const updateCategoryQuery =(id, values) =>{
-  return db.query ('UPDATE categories SET ? WHERE id = ?', [values, id]);
-}
-const deleteCategoryQuery = (id)=> {
-  return db.query ('DELETE FROM categories WHERE id = ? ', [id]);
-}
+const updateCategoryQuery = (id, values) => {
+  return db.query("UPDATE categories SET ? WHERE id = ?", [values, id]);
+};
+const deleteCategoryQuery = (id) => {
+  return db.query("DELETE FROM categories WHERE id = ? ", [id]);
+};
 
 module.exports = {
   addCategoryQuery,
-  getCategoriesQuery, 
-  getOneCategoryQueryById, 
-  getOneCategoryQueryByTitle, 
+  getCategoriesQuery,
+  getOneCategoryQueryById,
+  getOneCategoryQueryByTitle,
   getSelectedCategoriesQuery,
-  updateCategoryQuery, 
-  deleteCategoryQuery
-}
+  updateCategoryQuery,
+  deleteCategoryQuery,
+};
